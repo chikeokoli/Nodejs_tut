84eb3c9f06bb17b56f8e32c85dab692c1a2bdb4b
Chike Okoli <legendchyke@gmail.com>
2022-05-10 07:23:33 +0100

    another update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const _ = require('lodash')
 
 const items = [1,[1,2,[3,4,5]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+const newItemss = _.flattenDeep(items)
+console.log(newItemss)
